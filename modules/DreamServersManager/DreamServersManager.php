992577330bdd5ed63bb59793c1e252eb4d8d4ee9
konglf2112 <konglf2112@foxmail.com>
2018-12-28 01:55:48 +0800

    修改参与中梦想池获取为成功订单
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- modules/DreamServersManager/DreamServersManager.php
+++ modules/DreamServersManager/DreamServersManager.php
@@ -883,7 +883,7 @@ class DreamServersManager extends DBManager {
     public function GetJoinedPoolInfoByRange($uid,$min,$count){
         $link = $this->DBLink();
 
-        $sql = "SELECT `pid` FROM `order` WHERE `uid`='$uid'";
+        $sql = "SELECT `pid` FROM `order` WHERE `uid`='$uid' AND `state`='SUCCESS'";
 
         $tResult = DBResultToArray(mysql_query($sql,$link),true);
 
