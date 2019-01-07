1e475e1c5855b04e24651f6a8deb33172e56bd30
konglf2112 <konglf2112@foxmail.com>
2019-01-07 10:18:07 +0800

    增加新的后台页面存放目录
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

---
+++ admin/view/navagator.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-12-20
+ * Time: 上午 12:40
+ */
+    $navigators = [
+        'a_post'=>['title'=>'发布梦想池','class'=>'lnr lnr-home'],
+        'a_verify'=>['title'=>'中标用户审核','class'=>'lnr lnr-pencil'],
+    ];
+    foreach ($navigators as $key=>$value) {
+?>
+        <li>
+            <a id="<?php echo $key;?>" href="#" class="">
+                <i class="<?php echo $value['class'];?>"></i><span><?php echo $value['title'];?></span>
+            </a>
+        </li>
+<?php
+    }
+?>
+<script>
+    if(document.OnPartLoad) {
+        document.OnPartLoad(<?php echo json_encode($pageData);?>);
+    }
+</script>
