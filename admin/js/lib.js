d391206e1a8bc5b93a789d2a8de030644d706df3
konglf2112 <konglf2112@foxmail.com>
2019-01-16 01:53:22 +0800

    增加页面触底事件响应
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- admin/js/lib.js
+++ admin/js/lib.js
@@ -431,6 +431,26 @@ window.onbeforeunload = function (e) {
 	}
 };
 
+
+
+$(window).scroll(function () {
+    //已经滚动到上面的页面高度
+    var scrollTop = parseFloat($(this).scrollTop()),
+        //页面高度
+        scrollHeight = $(document).height(),
+        //浏览器窗口高度
+        windowHeight = parseFloat($(this).height()),
+        totalHeight = scrollTop + windowHeight;
+
+    //此处是滚动条到底部时候触发的事件，在这里写要加载的数据，或者是拉动滚动条的操作
+    if (scrollTop + windowHeight >= scrollHeight - 0.7) {
+        if(OnReachBottom){
+            OnReachBottom();
+        }
+    }
+}
+);
+
 var InitOptions = function () {
     if(ExistStorage("auth")){
         var auth = GetStorage("auth");
