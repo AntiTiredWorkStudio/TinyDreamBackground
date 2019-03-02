c9c789cc8cc5d7334618b34442e3b18c64719bbe
konglf2112 <konglf2112@foxmail.com>
2019-03-02 12:58:19 +0800

    取消分享弹窗
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- admin/js/webApp.js
+++ admin/js/webApp.js
@@ -293,14 +293,14 @@ var WebApp = {
 				imgUrl : config.imgUrl, // 分享图标
 				success : function () {
 					// 用户确认分享后执行的回调函数
-					alert('分享成功');
+					//alert('分享成功');
 					if(shareObject.OnShareTimeLine != null){
 						shareObject.OnShareTimeLine(true);
 					}
 				},
 				cancel : function () {
 					// 用户取消分享后执行的回调函数
-					alert('取消分享');
+					//alert('取消分享');
 					if(shareObject.OnShareTimeLine != null){
 						shareObject.OnShareTimeLine(false);
 					}
