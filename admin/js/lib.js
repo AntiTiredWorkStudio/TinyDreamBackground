45963c9dd187d34425fa347716feb841dc45c60c
konglf2112 <konglf2112@foxmail.com>
2019-05-05 15:10:21 +0800

    去掉请求失败弹窗
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- admin/js/lib.js
+++ admin/js/lib.js
@@ -151,7 +151,7 @@ var TD_Request = function(module,action,paras,fSuccess,fFailed) {
             }
         },
         error: function (err) {
-			alert("error:"+JSON.stringify(err));
+			//alert("error:"+JSON.stringify(err));
             fFailed('-1',err);
         }
     };
