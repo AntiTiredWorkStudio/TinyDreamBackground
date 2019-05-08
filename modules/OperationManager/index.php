310267e9b509d620e1aaf76468f057450416c9f0
konglf2112 <konglf2112@foxmail.com>
2019-05-09 00:55:40 +0800

    在
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- modules/OperationManager/index.php
+++ modules/OperationManager/index.php
@@ -21,7 +21,7 @@
 		'cls'=>R('ClearAllOAInfo'),//清理行动及打卡数据（包括：行动信息、打卡记录、邀请信息、行动订单）,参数：[uid:用户openid](可不加，不加为清除全部)
 		'gudo'=>R('GetUserDoingOperation',['uid','secret']),//获取用户正在参加的行动,参数:[uid:用户openid,secret:sha1("追梦行动派")]
         'reset'=>R('ResetOperation',['uid']),//重置用户正在进行的行动数据及打卡记录,参数:[uid:用户openid]
-        'gopd'=>R('GetOperationData',['state','seek','count']),//获取用户行动数据
+        'gopd'=>R('GetOperationData',['state','seek','count','#uid']),//获取用户行动数据
         'gatd'=>R('GetAttendenceData',['tele','seek','count']),//获取用户打卡数据
         'gind'=>R('GetInviteData',['seek','count']),//获取用户邀请记录
     ]);
